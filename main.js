419a2238c62e00537201cf62d776b7bd8402bc18
PetyaDeveloper <pskpetya@gmail.com>
2020-01-26 18:06:21 +0200

    small improvements

--- main.js
+++ main.js
@@ -2,23 +2,34 @@
 const nav = document.querySelector('.nav');
 let navLink = document.querySelectorAll('.nav a');
 console.log(navLink);
-//
-//nav.addEventListener('click',(e)=>{
-//    if(e.target.tagName === 'A'){
+
+//$('.nav').on('click', function(e){
+//   if(e.target.tagName === 'A'){
 //      for(let i=0; i<navLink.length; i++){
 //          navLink[i].removeAttribute('class');
 //          e.target.className = "selected";
 //      }
-//    }
+//      if(e.target.textContent !== 'Moto'){
+//          const originalText = `<img src="images/petya.jpg" alt="Picture of Petya Katsarova" class="profile-image">
+//                                <h1 class="tag name">Hello, I’m Petya.</h1>
+//                                <p class="tag location">You can find me in Dubai, UAE or somewhere around the world, travelling.</p>`
+//          document.getElementById('values').innerHTML = originalText;
+//      }
+//   }
 //});
+$('.chit-chat').on('click', function(e){
+   const replaceText = `<div><ul class="moto">
+                             <li>Let's make clean and beautiful code</li>
+                             <li>Let's have fun while doing it</li>
+                             <li>Keep growing</li>
+                        </ul></div>` 
+   document.getElementById('values').innerHTML = replaceText;
+});
 
+//// adding contact svg logos on the header
 $('.nav').on('click', function(e){
    if(e.target.tagName === 'A'){
-      for(let i=0; i<navLink.length; i++){
-          navLink[i].removeAttribute('class');
-          e.target.className = "selected";
-      }
-      if(e.target.textContent !== 'Values'){
+      if(e.target.textContent !== 'Moto'){
           const originalText = `<img src="images/petya.jpg" alt="Picture of Petya Katsarova" class="profile-image">
                                 <h1 class="tag name">Hello, I’m Petya.</h1>
                                 <p class="tag location">You can find me in Dubai, UAE or somewhere around the world, travelling.</p>`
@@ -26,9 +37,9 @@ $('.nav').on('click', function(e){
       }
    }
 });
-$('.chit-chat').on('click', function(e){
-   const replaceText = `<div><ul>
-                             <li>Let's make clean and beautiful code</li>
+$('.contact').on('click', function(e){
+   const replaceText = `<div id="contact"><ul>
+                             <li>svg1</li>
                              <li>Let's have fun while doing it</li>
                              <li>Keep growing</li>
                         </ul></div>` 
